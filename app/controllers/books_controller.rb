8102ab664994f5962393199605e5434d61ea2e30
Jeffrey Biñas <binasjeffrey34@yahoo.com>
2024-02-19 21:35:30 +0800

    One applied this commit will update some style of the web app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:bookno, :subject, :author, :title)
+      params.require(:book).permit(:bookno, :subject, :author, :title, :image)
     end
 end
